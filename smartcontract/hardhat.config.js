352a610bf9042032da9b6a3723d82eeeae7053cd
0xadamm <adam.mourade@gmail.com>
2022-09-17 18:46:09 -0700

    hardhat config done

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.7",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 };
