168a4618ebc6e5d874a3c83e0fc5a456a0b3589c
0xadamm <adam.mourade@gmail.com>
2022-09-17 23:53:00 -0700

    connected front end to moralis database

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./Coin.css";
 import { Button } from "web3uikit";
+import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
 
 function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
   const [color, setColor] = useState();
+  const contractProcessor = useWeb3ExecuteFunction();
+  const { isAuthenticated } = useMoralis();
 
   useEffect(() => {
     if (perc < 50) {
@@ -12,6 +15,36 @@ function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
       setColor("green");
     }
   }, [perc]);
+
+  async function vote(upDown) {
+    let options = {
+      contractAddress: "0x13dC58f28aaEb6f8bD5C715eDe4057D5707E9432",
+      functionName: "vote",
+      abi: [
+        {
+          inputs: [
+            { internalType: "string", name: "_ticker", type: "string" },
+            { internalType: "bool", name: "_vote", type: "bool" },
+          ],
+          name: "vote",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      params: { _ticker: token, _vote: upDown },
+    };
+
+    await contractProcessor.fetch({
+      params: options,
+      onSuccess: () => {
+        console.log("vote succesful");
+      },
+      onError: error => {
+        alert(error.data.message);
+      },
+    });
+  }
 
   return (
     <>
@@ -34,7 +67,11 @@ function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
         <div className="votes">
           <Button
             onClick={() => {
-              setPerc(perc + 1);
+              if (isAuthenticated) {
+                vote(true);
+              } else {
+                alert("Connect to Web3 to Vote");
+              }
             }}
             text="Up"
             theme="primary"
@@ -42,7 +79,11 @@ function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
           />
           <Button
             onClick={() => {
-              setPerc(perc - 1);
+              if (isAuthenticated) {
+                vote(false);
+              } else {
+                alert("Connect to Web3 to Vote");
+              }
             }}
             text="Down"
             theme="colored"
